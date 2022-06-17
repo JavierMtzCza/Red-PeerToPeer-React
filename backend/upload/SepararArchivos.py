f41c3026cadaee538711d2d816604808814b08c6
Javatt <javier.martinez.carranzaipn@gmail.com>
2022-06-17 00:14:21 -0500

    kkk

--- backend/upload/SepararArchivos.py
+++
@@ -1,7 +0,0 @@
-from os import system
-import sys 
-
-nombreArchivo = sys.argv[1]
-nombreSinExtension = nombreArchivo.split(".")
-
-system("split -n 3 "+ nombreArchivo + " " + nombreSinExtension[0])
